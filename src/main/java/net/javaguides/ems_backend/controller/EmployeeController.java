dd9cafd45e59fa95078164e8967051a60ca4fa64
satyam <satyam04sharma@gmail.com>
2024-09-15 01:51:12 -0700

    Update the @AllArgsConstructor for adding constructors to class variables

--- src/main/java/net/javaguides/ems_backend/controller/EmployeeController.java
+++ src/main/java/net/javaguides/ems_backend/controller/EmployeeController.java
@@ -1,15 +1,16 @@
 package net.javaguides.ems_backend.controller;
 
+import lombok.AllArgsConstructor;
 import net.javaguides.ems_backend.dto.EmployeeDto;
 import net.javaguides.ems_backend.service.EmployeeService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api/employee")
 public class EmployeeController {
