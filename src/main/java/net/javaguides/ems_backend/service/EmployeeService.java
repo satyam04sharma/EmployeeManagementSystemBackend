3c7dff2e124aa1000b233419d95e1d5fba181bc6
satyam <satyam04sharma@gmail.com>
2024-09-15 02:18:55 -0700

    Added all remaining rest API endpoints for EMS

--- src/main/java/net/javaguides/ems_backend/service/EmployeeService.java
+++ src/main/java/net/javaguides/ems_backend/service/EmployeeService.java
@@ -2,6 +2,10 @@ package net.javaguides.ems_backend.service;
 
 import net.javaguides.ems_backend.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
+    EmployeeDto getEmployeeById(Long id);
+    List<EmployeeDto> getAllEmployees();
 }
--- src/main/java/net/javaguides/ems_backend/service/impl/EmployeeServiceImpl.java
+++ src/main/java/net/javaguides/ems_backend/service/impl/EmployeeServiceImpl.java
@@ -3,10 +3,14 @@ package net.javaguides.ems_backend.service.impl;
 import lombok.AllArgsConstructor;
 import net.javaguides.ems_backend.dto.EmployeeDto;
 import net.javaguides.ems_backend.entity.Employee;
+import net.javaguides.ems_backend.exception.ResourseNotFoundException;
 import net.javaguides.ems_backend.mapper.EmployeeMapper;
 import net.javaguides.ems_backend.repository.EmployeeRepository;
 import net.javaguides.ems_backend.service.EmployeeService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -20,5 +24,20 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee savedEmployee = employeeRepository.save(employee);
         return EmployeeMapper.maptoEmployeeDto(savedEmployee);
 
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() ->
+                        new ResourseNotFoundException("Employee does not exist with ID:"+employeeId));
+        return EmployeeMapper.maptoEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map(EmployeeMapper::maptoEmployeeDto)
+                .collect(Collectors.toList());
     }
 }
